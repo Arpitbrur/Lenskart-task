225e7c9fcb3a0b9a3868904d1c7e151dec06c919
Arpit Brur <126546064+Arpitbrur@users.noreply.github.com>
2023-04-12 17:42:09 +0530

    add spring 1

--- spring-core-simple-xml-project/src/main/java/com/qsp/spring_core_simple_xml_project/Myspring1.java
+++ spring-core-simple-xml-project/src/main/java/com/qsp/spring_core_simple_xml_project/Myspring1.java
@@ -2,4 +2,36 @@ package com.qsp.spring_core_simple_xml_project;
 
 public class Myspring1 {
 
+	private int id;
+	private String Name;
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Myspring1(int id, String name, String email) {
+		super();
+		this.id = id;
+		Name = name;
+		this.email = email;
+	}
+	
+	
+	
 }
